9746abc2f9b788e73a5292fe45df036eccc1b594
arefin6 <arefinhossain3@gmail.com>
2022-04-18 14:58:08 +0600

    Db Connection done

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import  mongoose  from 'mongoose'
+
+
+const connectDb = async ()=>{
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI)  
+      console.log(`MongoDb Connected ${conn.connection.host}`)
+    } catch (error) {
+        
+        console.log(`Error ${error.message}`)
+        process.exit(1)
+    }
+}
+
+export default connectDb;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
+import dotEnv from 'dotenv';
+import connectDb from './config/db.js';
 
+dotEnv.config();
+connectDb();
 const app = express()
 app.use ( express.json());
 
 const Port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('API is running....')
+    res.send('API is running....');
 })
 
 
